572f25a6ca25148ee856eb6823c7a04b96127763
Ben Ewing <benewing1992@gmail.com>
2016-03-16 14:11:52 -0700

    Switched examples around.

--- examples/example_2.R
+++ examples/example_2.R
@@ -23,9 +23,8 @@ load("datasets/usaid_data.rdata")
 library(plyr)
 # Caret of course
 library(caret)
-# ggplot2 for plots, ggthemes for nice themes 
+# ggplot2 for plots
 library(ggplot2)
-library(ggthemes)
 # randomForest for...random forests
 library(randomForest)
 #==================================================================================================
@@ -54,56 +53,67 @@ library(randomForest)
 # Set up testing data and training data
 set.seed(3456)
 
-trainIndex <- createDataPartition(y = eerf$parc_2cer, p = 0.8, list = F)
-train_data <- eerf[trainIndex,]
-test_data  <- eerf[-trainIndex,]
+trainIndex <- createDataPartition(y = eerf$parc_2cer_num, p = 0.8, list = F)
+training <- eerf[trainIndex,]
+testing <- eerf[-trainIndex,]
 #==================================================================================================
 
 #==================================================================================================
-# for (i in 1:ncol(train_data)) {
-#   print(names(train_data)[i])
-#   if (class(train_data[, names(train_data)[i]])[1] == "factor")
-#     contrasts(train_data[, names(train_data)[i]])
+# for (i in 1:ncol(training)) {
+#   print(names(training)[i])
+#   if (class(training[, names(training)[i]])[1] == "factor")
+#     contrasts(training[, names(training)[i]])
 # }
 #==================================================================================================
 
 #==================================================================================================
 # Training Model
 # Basic - Use whichever variables you'd like to test here
-mod_rf <- train(parc_2cer ~ state_id + hh_size + hh_num_male + hh_num_female +
-                 hh_head_age + hh_avg_age + hh_ed_avg_yrs + inc_farm1 + inc_manual1 + 
-                 inc_domstc1 + lofftrtime + redland, 
-                data = train_data,
-                trControl = trainControl(method="cv",number=5),
-                metric = "Kappa",
-                method = "rf")
+system.time(mod_rf1 <- train(parc_2cer_num ~ state_id + hh_size + hh_num_male + hh_num_female +
+                              hh_head_age + hh_avg_age + hh_ed_avg_yrs + inc_farm1 + inc_manual1 + 
+                              inc_domstc1 + lofftrtime + redland, 
+                            data = training,
+                            trControl = trainControl(method="cv",number=5),
+                            #prox = TRUE, 
+                            metric = "Kappa",
+                            method = "rf"))
+
+system.time(mod_rf2 <- train(parc_2cer_num ~ state_id + hh_size + hh_num_male + hh_num_female +
+                              hh_head_age + hh_avg_age + hh_ed_avg_yrs + inc_farm1 + inc_manual1 + 
+                              inc_domstc1 + lofftrtime + redland, 
+                            data = training,
+                            #trControl=trainControl(method="cv",number=5),
+                            #prox = TRUE, 
+                            metric = "Kappa",
+                            method = "rf"))
+# Long
+# system.time(modelFitLong <- train(dispute ~ .,
+#                       data = train_data, 
+#                       method = "rf", 
+#                       prox = TRUE))                    
 #==================================================================================================
 
 #==================================================================================================
-# Evalutating the model on its own
-# By just typing the model name, we can get some information
-mod_rf
-# Or to see some info about the final model, and particularly the confusion matrix
-mod_rf$finalModel
-#==================================================================================================
+# Testing Model on Our Predictions
+pred1 <- predict(mod_rf1, testing)
+pred2 <- predict(mod_rf2, testing)
 
-#==================================================================================================
-# Evaluating our Model on Our Predictions
-pred <- predict(mod_rf, test_data)
 
-confusionMatrix(pred, test_data$parc_2cer)
+testing$predRight1 <- pred1==testing$parc_2cer_num
+testing$predRight2 <- pred2==testing$parc_2cer_num
 
-test_data$predRight <- pred==test_data$parc_2cer
-qplot(parc_2cer, fill=predRight, data=test_data, main="", 
-      xlab = "Second Level Certification") + theme_fivethirtyeight()
-#==================================================================================================
 
-#==================================================================================================
-# Variable Importance
-varImp(mod_rf)
+table(testing$predRight1)
+prop.table(table(testing$predRight1))
 
-# and
-ggplot(varImp(mod_rf)) + theme_fivethirtyeight()
-# or
-plot(varImp(mod_rf))
+table(testing$predRight2)
+prop.table(table(testing$predRight2))
+
+pred1_plot <- qplot(parc_2cer_num, fill=predRight1, data=testing,main="Simple Resample", 
+                    xlab = "Second Level Certification")
+pred2_plot <- qplot(parc_2cer_num, fill=predRight2, data=testing,main="Not Simple Resample", 
+                    xlab = "Second Level Certification")
+
+library(gridExtra)
+grid.arrange(pred1_plot, pred2_plot, ncol = 2)
 #==================================================================================================
